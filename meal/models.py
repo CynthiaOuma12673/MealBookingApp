8131d03927fc2fee13993d1bb24cd398eefe1f9d
Francisflow <francisngigi948@gmail.com>
2022-04-21 14:16:07 +0300

    Profile view function

--- meal/models.py
+++ meal/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
+
+
+
+class Profile(models.Model):
+    profile_pic = CloudinaryField('image')
+    
