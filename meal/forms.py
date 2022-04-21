8dfb7215fc14de33fbd5b3512489d82958dcfc96
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-04-21 09:09:44 +0300

    it will create the base file

--- meal/forms.py
+++ meal/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Post,Rating,Profile
+from .models import Rating,Profile
 from django.forms.widgets import Textarea
 
 class UserRegisterForm(UserCreationForm):
